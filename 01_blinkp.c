bd7c5861d501bb67926fbb0c403d962007952119
Ronald Ajila <rdajila@gmail.com>
2018-10-05 13:44:52 +0000

    LED RED PWD

---
+++ 01_blinkp.c
@@ -0,0 +1,33 @@
+#include <wiringPi.h>
+#include <stdio.h>
+
+/*
+ * gcc -Wall -o <nombre> <nombre>.c -lwiringPi
+ * sudo ./<nombre>
+ *
+ * Link ayuda:
+ * */
+
+#define LRED 1
+#define MAX 1024
+
+int main (void)
+{
+	wiringPiSetup();
+	pinMode(LRED, PWM_OUTPUT);
+
+	for(;;){
+		for(int i=0;i<MAX;i++)
+		{
+			pwmWrite(LRED,i);
+			delay(1);
+		}
+		delay(1000);
+		for(int i=MAX-1;i>=0;i--)
+		{
+			pwmWrite(LRED,i);
+			delay(1);
+		}
+	}
+	return 0;
+}
