b7ed6974cb22f6beeb867d780594fb43de72efb2
Ronald <rdajila@gmail.com>
2018-12-14 16:00:10 +0100

    Init Project FoodDispenserMonitoring

---
+++ proyectoTwo/FoodDispenserMonitoring/app/src/main/java/es/upm/android/iot/jdelatorre/rdajila/fooddispensermonitoring/FoodDispenserMonitoring.java
@@ -0,0 +1,23 @@
+package es.upm.android.iot.jdelatorre.rdajila.fooddispensermonitoring;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FoodDispenserMonitoring extends Activity
+{
+    private static final String TAG = FoodDispenserMonitoring.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+    }
+
+}
