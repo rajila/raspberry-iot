7bef5337e4ff61bba1587bd8a2bd00b002ca475b
Ronald <rdajila@gmail.com>
2019-01-06 13:13:18 +0100

    Start app FoodWaterDisoensing on Android Thing

---
+++ proyectFinalTwo/FoodWaterDispensing/app/src/main/java/es/upm/android/iot/jdelatorre/rdajila/foodwaterdispensing/FoodWaterDispensing.java
@@ -0,0 +1,20 @@
+package es.upm.android.iot.jdelatorre.rdajila.foodwaterdispensing;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FoodWaterDispensing extends Activity {
+    private static final String TAG = FoodWaterDispensing.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+}
